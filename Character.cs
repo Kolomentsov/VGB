9efaeca3a8f10492f909b15dd98c90a48f5050a6
Angelina9816 <ange.16@yandex.ru>
2017-12-15 22:50:28 +0300

    Add files via upload

---
+++ Character.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using IGamesData.GamesData;
+
+namespace IGamesData
+{
+    public class Character
+    {
+        public List<int> Games { get; set; }
+
+        public string Descriprtion { get; set; }
+        public string Name { get; set; }
+
+        public string Poster { get; set; }
+    }
+}
